664a69e13aee0b772c8af0b1b3f2992af77ce0ab
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2022-09-19 12:06:24 +0200

    passing arguments nodejs

--- livecodes/2.nodejs-passing-arguments/index.js
+++ livecodes/2.nodejs-passing-arguments/index.js
@@ -1,12 +1,44 @@
 //import chalk from 'chalk';
-import {getBanana,getNotes} from "./notes.js";
+//import {getBanana,getNotes} from "./notes.js";
 
 
-const msg = banana()
+//const msg = banana()
 
 //const banana = getBanana()
 
-console.log(msg);
+//console.log(msg);
 //console.log(banana);
 
 // console.log(chalk.bgRed.blue('Hello world!'));
+
+//const argvs = process.argv.slice(2) // ['/../../node', '/User/dilshod/../index.js']
+
+// const numbers = argvs.map(num=>parseInt(num))
+
+// console.log(numbers);
+
+//console.log(argvs); 
+
+const argvs = process.argv.slice(2);
+
+//console.log(argvs[0]);
+
+const func1 = () => {
+    console.log("Hi, I am function 1");
+}
+
+const func2 = () => {
+    console.log("Hi, I am function 2");
+}
+
+switch (argvs[0]) {
+    case 'func1':
+        func1()
+        break;
+    case 'func2':
+        func2()
+        break;
+    default:
+        break;
+}
+
