d0208ef81bf4bbcb8e307863470b04941555730a
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2022-09-26 13:58:11 +0200

    express router live code

---
+++ livecodes/5.express-router/routes/items.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+let router = express.Router();
+
+//http://localhost:5000/items/books
+
+router
+  .route("/books")
+  .get((req, res) => {
+    res.send("This is GET request and url is /items/books");
+  })
+  .post((req, res) => {
+    res.send("Post request. Url /items/books ...");
+  });
+
+router
+  .route("/books/:bookid")
+  .get((req, res) => {
+    res.send(`Book id is: ${req.params.bookid}`);
+  })
+  .put((req, res) => {
+    res.send(`Book with id ${req.params.bookid} updated!`);
+  });
+
+export default router;
--- livecodes/5.express-router/server.js
+++ livecodes/5.express-router/server.js
@@ -1,25 +1,42 @@
 import express from "express";
-import { auth } from "./middleware/auth.js";
+//import { auth } from "./middleware/auth.js";
+import items from "./routes/items.js";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+//GET and POST
+app.use("/items", items);
+// app
+//   .route("/items/books")
+//   .get((req, res) => {})
+//   .post((req, res) => {});
+
+// app
+//   .route("/items/books/:bookid")
+//   .get((req, res) => {})
+//   .put((req, res) => {});
+
 //http://localhost:5000/
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
+// app.post("/login", (req, res) => {
+//   const { username, password } = req.body;
 
-  if (username === "dilshod" && password === "1234") {
-    res.send(`Login success! Hello ${username}`);
-  } else {
-    res.send("Login failed!");
-  }
-});
+//   if (username === "dilshod" && password === "1234") {
+//     res.send(`Login success! Hello ${username}`);
+//   } else {
+//     res.send("Login failed!");
+//   }
+// });
 
-app.get("/foo/:id", auth, (req, res) => {
-  const id = req.userId;
-  res.send(`The user id is: ${id}`);
-});
+// app.get("/foo/:id", auth, (req, res) => {
+//   const id = req.userId;
+//   res.send(`The user id is: ${id}`);
+// });
 
 app.listen(5000, () => console.log("Port is listening: 5000"));
