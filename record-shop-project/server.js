15296b7e8bfa2c0d4797f8ca1c5c1c4d7f5a41dd
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2022-10-09 23:30:03 +0200

    solution task2 and task3

--- record-shop-project/server.js
+++ record-shop-project/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+
+import logger from "morgan";
+import cors from "cors";
+
+import ordersRouter from "./routes/orders.js";
+import recordsRouter from "./routes/records.js";
+import usersRouter from "./routes/users.js";
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
 
 //DB ----------------------------------
 
@@ -20,22 +31,37 @@ const db = new Low(adapter);
 
 await db.read();
 
-db.data ||= { records: [] };
+db.data ||= { records: [], users: [], orders: [] };
 //
 //--------------------------------------
 
 //Create
-app.post("/api/records", async (req, res, next) => {
-  const { records } = db.data;
-  records.push({ ...req.body, id: Date.now().toString() });
-  await db.write();
-  res.send(records);
-});
+// app.post("/api/records", async (req, res, next) => {
+//   const { records } = db.data;
+//   records.push({ ...req.body, id: Date.now().toString() });
+//   await db.write();
+//   res.send(records);
+// });
 
 //Read
-app.get("/api/records", (req, res, next) => {
-  const { records } = db.data;
-  res.send(records);
+// app.get("/api/records", (req, res, next) => {
+//   const { records } = db.data;
+//   res.send(records);
+// });
+//ROUTES
+
+app.use("/orders", ordersRouter);
+app.use("/records", recordsRouter);
+app.use("/users", usersRouter);
+
+//GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
 });
 
 const port = process.env.PORT || 5000;
